4b534422fd8fc85df9abf3c774c2a15ab25f8362
matpitas <pitas.matheus4@gmail.com>
2023-08-29 16:44:03 -0300

    att

--- frontend/src/pages/Painel/Painel.jsx
+++ frontend/src/pages/Painel/Painel.jsx
@@ -3,7 +3,29 @@ import styles from './Painel.module.css'
 
 const Painel = () => {
   return (
-    <div>Painel</div>
+    <div className={styles.painel}>
+        <div className={styles.ultimoJogo}>
+          <div className={styles.imagemUltimoJogo}>
+            <img src="" alt="" />
+          </div>
+          <div className={styles.contextoUltimoJogo}>
+            <p>Ultimo jogo com os amigos</p>
+            <h1>Valorant</h1>
+            <span>Jogo de Tiro</span>
+            {/* amigos */}
+            <button>Jogar Novamente</button>
+          </div>
+        </div>
+        <div className={styles.proximaJogatina}>
+          
+        </div>
+        <div className={styles.listaAmigos}>
+
+        </div>
+        <div className={styles.iniciar}>
+          
+        </div>
+    </div>
   )
 }
 
