99a1e79ba6140b1ca7c478367787dcb6242af9e6
matpitas <pitas.matheus4@gmail.com>
2023-10-11 11:14:35 -0300

    .

--- frontend/src/pages/Cadastrar/Cadastrar.jsx
+++ frontend/src/pages/Cadastrar/Cadastrar.jsx
@@ -16,93 +16,81 @@ const Cadastrar = () => {
   const handleAvatarChange = (e) => {
     const file = e.target.files[0]
 
-    if(file){ 
+    if (file) {
       setAvatar(file)
-    } 
+    }
   }
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const formData = new FormData()
-    formData.append("nome",   nome)
-    formData.append("email",  email)
-    formData.append("senha",  senha)
-    formData.append("avatar", JSON.stringify(avatar))
-
-    if(nome && email && senha && conSenha && avatar){
-      if(senha === conSenha){
-        
-        await axios({
-          method: 'post',
-          headers:{ 
-            'Content-Type': 'multipart/form-data'
-          },
-          url: 'http://localhost:3333/users/create',
-          data:{
-            nome,
-            senha,
-            email,
-            avatar,
-          }
-        }).then((response) => {
-          if(response.data.ID){
-            toast.success(
-              "Cadastro efetuado com sucesso"
-            )
-          }else{
-            toast.error(
-              "Houve um erro no seu cadastro."
-            )
-          }
-        }).catch(e => {
-          toast.error(e)
-        })
-      }else{
-        toast.error(
-          "Senhas não são iguais"
-        )
-      }
-    }else{
-      toast.error(
-        "Há campos não preenchidos"
-      )
+    if (!nome || !email || !senha || !conSenha || !avatar) {
+      toast.error('Erro: preencha todos os campos.');
+      return;
     }
 
-    
+    if (senha !== conSenha) {
+      toast.error('Erro: senhas não correspondem.');
+      return;
+    }
+
+    await axios({
+      method: 'post',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      url: 'http://localhost:3333/users/create',
+      data: {
+        nome,
+        senha,
+        email,
+        avatar,
+      }
+    }).then((response) => {
+      if (!response.data.ID) {
+        toast.error("Houve um erro no seu cadastro.")
+        return;
+      }
+
+      toast.success(
+        "Cadastro efetuado com sucesso"
+      )
+    }).catch(e => {
+      toast.error(e)
+    })
   }
 
   return (
     <form encType="multipart/form-data" className={styles.caixaCadastro} onSubmit={(e) => handleSubmit(e)}>
-        <h1>Cadastro</h1>
+      <h1>Cadastro</h1>
 
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>Nome</label>
+        <input type="text" placeholder='Matheus Pitas Baptista' className={styles.input} value={nome} onChange={(e) => setNome(e.target.value)} />
+      </div>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>Email</label>
+        <input type="text" placeholder='email@email.com' className={styles.input} value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div className={styles.passwordGrid}>
         <div className={styles.inputContainer}>
-          <label className={styles.label}>Nome</label>
-          <input type="text" placeholder='Matheus Pitas Baptista' className={styles.input} value={nome} onChange={(e) => setNome(e.target.value)} />
-        </div>
-        <div className={styles.inputContainer}>
-          <label className={styles.label}>Email</label>
-          <input type="text" placeholder='email@email.com' className={styles.input} value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className={styles.passwordGrid}>
-          <div className={styles.inputContainer}>
-            <label className={styles.label}>Senha</label>
-            <input type="text" placeholder='******' className={styles.input} value={senha} onChange={(e) => setSenha(e.target.value)} />
-          </div>
-          <div className={styles.inputContainer}>
-            <label className={styles.label}>Confirmar Senha</label>
-            <input type="text" placeholder='******' className={styles.input} value={conSenha} onChange={(e) => setConSenha(e.target.value)} />
-          </div>
+          <label className={styles.label}>Senha</label>
+          <input type="text" placeholder='******' className={styles.input} value={senha} onChange={(e) => setSenha(e.target.value)} />
         </div>
         <div className={styles.inputContainer}>
-            <label className={styles.label}>Avatar</label>
-            <input type="file" className={styles.input} accept="image/*" value="" onChange={(e) => handleAvatarChange(e)} />
+          <label className={styles.label}>Confirmar Senha</label>
+          <input type="text" placeholder='******' className={styles.input} value={conSenha} onChange={(e) => setConSenha(e.target.value)} />
         </div>
-        
+      </div>
+      <div className={styles.inputContainer}>
+        <label className={styles.label}>Avatar</label>
+        <input type="file" title='Escolha uma Imagem' className={styles.input} accept="image/*" value="" onChange={(e) => handleAvatarChange(e)} />
+      </div>
 
-        
-        <button type='submit' className={styles.cadastro}>Cadastrar</button>
-        <Link to="/entrar" className={styles.entrar}>Voltar</Link> 
+
+
+      <button type='submit' className={styles.cadastro}>Cadastrar</button>
+      <Link to="/entrar" className={styles.entrar}>Voltar</Link>
     </form>
   )
 }
